fc90604d8070fc68159d87bef1250071a9fe7ffe
SULEYMAN DIKER <TCSDIKER@turkcell.entp.tgc>
2021-02-18 14:39:19 +0300

    updated codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,96 @@
-import "./styles.css";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { debounce } from "throttle-debounce";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+// import getAll from './data';
+import ListBooks from "./ListBooks";
+import SearchBooks from "./SearchBooks";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class BooksApp extends Component {
+  bookshelves = [
+    { key: "currentlyReading", name: "Currently Reading" },
+    { key: "wantToRead", name: "Want to Read" },
+    { key: "read", name: "Read" }
+  ];
+  state = {
+    myBooks: [],
+    searchBooks: []
+  };
+  componentDidMount = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ myBooks: books });
+    });
+  };
+  moveBook = (book, shelf) => {
+    // update db
+    BooksAPI.update(book, shelf);
+    // BooksAPI.update(book, shelf).then(books => {
+    //   console.log(books);
+    // });
+
+    let updatedBooks = [];
+    updatedBooks = this.state.myBooks.filter((b) => b.id !== book.id);
+
+    if (shelf !== "none") {
+      book.shelf = shelf;
+      updatedBooks = updatedBooks.concat(book);
+    }
+
+    // console.log('updated books len', updatedBooks.length);
+    this.setState({
+      myBooks: updatedBooks
+    });
+  };
+  searchForBooks = debounce(300, false, (query) => {
+    //console.log(query);
+    if (query.length > 0) {
+      BooksAPI.search(query).then((books) => {
+        //   console.log('result', books);
+        if (books.error) {
+          this.setState({ searchBooks: [] });
+        } else {
+          this.setState({ searchBooks: books });
+        }
+      });
+    } else {
+      this.setState({ searchBooks: [] });
+    }
+  });
+  resetSearch = () => {
+    this.setState({ searchBooks: [] });
+  };
+
+  render() {
+    const { myBooks, searchBooks } = this.state;
+    return (
+      <div className="app">
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListBooks
+              bookshelves={this.bookshelves}
+              books={myBooks}
+              onMove={this.moveBook}
+            />
+          )}
+        />
+        <Route
+          path="/search"
+          render={() => (
+            <SearchBooks
+              searchBooks={searchBooks}
+              myBooks={myBooks}
+              onSearch={this.searchForBooks}
+              onMove={this.moveBook}
+              onResetSearch={this.resetSearch}
+            />
+          )}
+        />
+      </div>
+    );
+  }
 }
+
+export default BooksApp;
