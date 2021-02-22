017a85c2fb25ae83fcb9a75ba351a68e7ce5c5ab
suleymandiker <suleyman.diker@gmail.com>
2021-02-22 15:27:46 +0300

    Update index.js
    added comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
+//First Project
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <BrowserRouter>
